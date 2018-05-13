954cdf7a431b3b081e671edafb7304b553763e6c
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-13 21:59:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const token = ''
+
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
+
+
 function getIssues() {
 }
 
